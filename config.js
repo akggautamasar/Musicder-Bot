3ff1ce1ae23ae33f1149e10521f91d74ec7e2e7d
akggautamasar <Worksbeyondworks@gmail.com>
2025-05-30 04:46:13 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  bot_api_key: "YOUR_BOT_API_KEY_HERE", // replace with your bot token
+
+  // JioSaavn API URL (you can self-host or use public)
+  jiosaavn_api_url: "https://jiosaavn-api.vercel.app/",
+
+  // Musicder download URL (optional, if you want to add)
+  musicder_url: "https://musicder-prod.vercel.app/",
+
+  start_message: `<b>Hey, 👋👋</b>\n\n<em>Welcome to <b>Akgsmusic Bot</b>.\n\nJust send the Song's Name or JioSaavn Link & get best matched results with download links.\n\nBrought to you by @tprojects</em>`,
+
+  about_message: `Created by <a href='https://tu.in.life'>Air</a>.`,
+
+  help_message: `Send me a song name or JioSaavn link and I will provide download links for the best matching songs.`,
+
+};
